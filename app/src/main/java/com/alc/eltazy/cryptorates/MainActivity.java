770512a33ae125ab81135be4c982ab7fde864461
Michel Buhendwa <mkbuhendwa@gmail.com>
2017-11-08 16:06:58 +0300

    Initial commit - Cryptocompare API

---
+++ app/src/main/java/com/alc/eltazy/cryptorates/MainActivity.java
@@ -0,0 +1,364 @@
+package com.alc.eltazy.cryptorates;
+
+// Import statements
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+class RateItem {
+    //Class for Objects to handle rate data
+    private String icon = null;
+    private String symbol = null;
+    private Double value = null;
+
+    public void setIcon(String t_icon) {
+        this.icon = t_icon;
+    }
+    public String getIcon() {
+        return this.icon;
+    }
+    public void setSymbol(String t_symbol) {
+        this.symbol = t_symbol;
+    }
+    public String getSymbol() {
+        return this.symbol;
+    }
+    public void setValue(Double t_value) {
+        this.value = t_value;
+    }
+    public Double getValue() {
+        return this.value;
+    }
+}
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+    //UI widgets
+    private ImageButton settingsButton = null;
+    private Button refreshButton = null;
+    private ListView list_view = null;
+    private Spinner spinner_base_coin = null;
+    private Spinner spinner_pref_currency = null;
+    private TextView pref_exchange_rate = null;
+    private TextView pref_symbol = null;
+    private ProgressBar refresh_progress = null;
+    //Class attributes
+    private String base_currency = null;
+    private String preference_currency = null;
+    private Double preference_rate = .0;
+    //HTTP Request data containers
+    private HashMap<String, Double> rates = null;
+    private ArrayList<RateItem> view_rates = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Retrieving user settings if saved before or
+        // Return default app settings
+        SharedPreferences myPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String coin_setting = myPreferences.getString(getString(R.string.pref_coin_setting), getString(R.string.default_coin));
+        setBaseCurrency(coin_setting);
+        String currency_setting = myPreferences.getString(getString(R.string.pref_currency_setting), getString(R.string.default_currency));
+        setPreferenceCurrency(currency_setting);
+
+        // Setting UI elements
+        refresh_progress = findViewById(R.id.refresh_progress);
+        list_view = findViewById(R.id.custom_list);
+        spinner_base_coin = findViewById(R.id.base_currency_spinner);
+        spinner_pref_currency = findViewById(R.id.preference_currency_spinner);
+        refreshButton = findViewById(R.id.refresh_button);
+        settingsButton = findViewById(R.id.settings_button);
+        pref_symbol = findViewById(R.id.preference_symbol);
+        pref_exchange_rate = findViewById(R.id.preference_rate);
+
+        // Initializing UI with user settings if existing
+        // or with default values
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.pref_list_coin_values, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_base_coin.setAdapter(adapter);
+        spinner_base_coin.setSelection(adapter.getPosition(coin_setting));
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.pref_list_currency_values, android.R.layout.simple_spinner_item);
+        spinner_pref_currency.setAdapter(adapter);
+        spinner_pref_currency.setSelection(adapter.getPosition(currency_setting));
+
+        pref_symbol.setText(getCurrencySymbol(preferenceCurrency()));
+        pref_exchange_rate.setText(preferenceRate().toString());
+
+        //Setting Listeners
+        spinner_base_coin.setOnItemSelectedListener(this);
+        spinner_pref_currency.setOnItemSelectedListener(this);
+        refreshButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+
+        //Retrieving CRYPTOCOMPARE API Data based on default settings
+        // or user settings
+        retrievingData();
+    }
+
+    //Listeners
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.refresh_button:
+                // If user pressed the Refresh button
+                retrievingData();
+                break;
+            case R.id.settings_button:
+                // If user pressed the Settings button
+                Intent settings_intent = new Intent(this, SettingsActivity.class);
+                startActivity(settings_intent);
+                break;
+        }
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // Refresh Data if user changed options in spinners
+        switch (parent.getId()){
+            case R.id.base_currency_spinner:
+                setBaseCurrency(parent.getSelectedItem().toString());
+                retrievingData();
+                break;
+            case R.id.preference_currency_spinner:
+                setPreferenceCurrency(parent.getSelectedItem().toString());
+                pref_exchange_rate.setText(preferenceRate().toString());
+                pref_symbol.setText(getCurrencySymbol(preferenceCurrency()));
+                retrievingData();
+                break;
+        }
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){}
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view,int pos, long id){
+    }
+
+    //Setters and Getters
+    public String baseCurrency(){
+        return base_currency;
+    }
+    public String preferenceCurrency(){
+        return preference_currency;
+    }
+    public Double preferenceRate(){
+        return preference_rate;
+    }
+    public void setBaseCurrency(String b){
+        base_currency = b;
+    }
+    public void setPreferenceCurrency(String b){
+        preference_currency = b;
+    }
+    public void setPreferenceRate(Double b){
+        preference_rate = b;
+    }
+
+
+    //other class methods
+    private ArrayList getListData() {
+        // Function to return CRYPTOCOMPARE API Data
+        // formatted as an array of RateItem Objects
+        ArrayList<RateItem> results = new ArrayList<>();
+        for(HashMap.Entry<String, Double> entry: rates.entrySet()) {
+            RateItem item = new RateItem();
+            String code = entry.getKey();
+
+            item.setIcon(code);
+            item.setSymbol(getCurrencySymbol(code));
+            item.setValue(entry.getValue());
+            results.add(item);
+        }
+        return results;
+    }
+    protected  String getCurrencySymbol(String code){
+        switch (code){
+            case "BTC": return "BTC";
+            case "ETH": return "ETH";
+            case "JPY": return "¥";
+            case "EUR": return "€";
+            case "GBP": return "£";
+            case "NGN": return "₦";
+            case "CHF": return "Fr";
+            case "KRW": return "₩";
+            case "CNY": return "元";
+            case "RUB": return "\u20BD";
+            case "INR": return "₹";
+            case "SEK": case "NOK": return "kr";
+            case "TRK": case "TRY": return "₺";
+            default: return "$";
+        }
+    }
+    public void refresh() {
+        // Show ListView and hide progressBar
+        refresh_progress.setVisibility(View.INVISIBLE);
+        list_view.setVisibility(View.VISIBLE);
+        list_view.setAdapter(new CustomAdapter(this, view_rates));
+        // Set rate and currency symbol to highlight
+        pref_symbol.setText(getCurrencySymbol(preferenceCurrency()));
+        pref_exchange_rate.setText(rates.get(preferenceCurrency()).toString());
+    }
+    public void retrievingData() {
+        if(hasInternetConnectivity()){
+            // Hides ListView and shows ProgressBar as the API request is sent
+            new RetrieveHTTPSRequestData().execute();
+            list_view.setVisibility(View.INVISIBLE);
+            refresh_progress.setVisibility(View.VISIBLE);
+        }
+        //Notifies on a Toast "No Internet" message
+        else Toast.makeText(MainActivity.this, "No Internet.\nConnect to the Internet then Refresh", Toast.LENGTH_LONG).show();
+    }
+    public boolean hasInternetConnectivity() {
+        // Returns true or false whether or not there is Internet connexion
+        ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivity.getActiveNetworkInfo();
+        if (activeNetwork == null) return false;
+        else if (!activeNetwork.isConnected()) return false;
+        else return true;
+    }
+//Sub-classes
+    // ListView Custom Adapter to help display
+    // in list the icon, the symbol and exchange rate
+    class CustomAdapter extends BaseAdapter{
+        private ArrayList<RateItem> listData = null;
+        private LayoutInflater layoutInflater = null;
+
+        public CustomAdapter(Context context, ArrayList<RateItem> listData) {
+            this.listData = listData;
+            this.layoutInflater = LayoutInflater.from(context);
+        }
+        @Override
+        public int getCount() {
+            return listData.size();
+        }
+        @Override
+        public Object getItem(int i) {
+            return listData.get(i);
+        }
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.list_item_style, null);
+                holder = new ViewHolder();
+                holder.icon = convertView.findViewById(R.id.icon);
+                holder.symbol = convertView.findViewById(R.id.symbol);
+                holder.value = convertView.findViewById(R.id.value);
+                convertView.setTag(holder);
+            }
+            else holder = (ViewHolder) convertView.getTag();
+
+            String temp_id = listData.get(position).getIcon().toLowerCase();
+            holder.icon.setImageResource(getResources().getIdentifier(temp_id, "drawable", getPackageName()));
+            holder.symbol.setText(listData.get(position).getSymbol());
+            holder.value.setText(listData.get(position).getValue().toString());
+            return convertView;
+        }
+        class ViewHolder {
+            ImageView icon;
+            TextView symbol;
+            TextView value;
+        }
+    }
+    //Asynchronous https request connexion thread
+    class RetrieveHTTPSRequestData extends AsyncTask<Void, Void, String> {
+        protected void onPreExecute() {}
+        protected String doInBackground(Void... urls) {
+            // Workaround as try is a java reserved word
+            // Replacing it with trk for turkey
+            String curr;
+            if (baseCurrency() == "TRK") curr = "TRY";
+            else curr = baseCurrency();
+            // Based on Wikipedia's world top 20 most traded currencies
+            // with South Africa's Rand replaced by Nigeria's Naira (NGN)
+            String str_url = "https://min-api.cryptocompare.com/data/price?fsym=" + curr +
+                    "&tsyms=BTC,ETH,USD,EUR,JPY,GBP,AUD,CAD,CHF,CNY,SEK,MXN,NZD,SGD,HKD,NOK,KRW,TRY,INR,RUB,BRL,NGN";
+            try {
+                // Sending Request
+                URL url = new URL(str_url);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(bufferedReader.readLine());
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    // Closing connexion
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                //Handling Exception Errors
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            //Statements to execute after request returned a response
+            if(response == null) response = "THERE WAS AN ERROR";
+            Log.i("INFO", response);
+            try {
+                //Saving response in a JSON Object
+                JSONObject object = new JSONObject(response.replace("TRY", "TRK"));
+                HashMap<String, Double> rate_list = new HashMap<>();
+
+                // Saving values for each keys in JSON Object
+                Iterator<String> keysItr = object.keys();
+                while(keysItr.hasNext()) {
+                    String key = keysItr.next();
+                    Double value = object.getDouble(key);
+                    rate_list.put(key, value);
+                }
+                // Initializing data arrays
+                rates = rate_list;
+                view_rates = getListData();
+                // Updating preference data
+                setPreferenceRate(rate_list.get(preferenceCurrency()));
+                // Displaying Rates
+                refresh();
+            } catch (JSONException e) {
+                // Error handling
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/alc/eltazy/cryptorates/SettingsActivity.java
@@ -0,0 +1,112 @@
+package com.alc.eltazy.cryptorates;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    /**
+     * A preference value change listener that updates the preference's summary
+     * to reflect its new value.
+     */
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+            }
+            else{
+
+            }
+            return true;
+        }
+    };
+
+    /**
+     * Binds a preference's summary to its value. More specifically, when the
+     * preference's value is changed, its summary (line of text below the
+     * preference title) is updated to reflect the value. The summary is also
+     * immediately updated upon calling this method. The exact display format is
+     * dependent on the type of preference.
+     *
+     * @see #sBindPreferenceSummaryToValueListener
+     */
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        // Trigger the listener immediately with the preference's current value.
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
+    }
+
+    /**
+     * This method stops fragment injection in malicious applications.
+     * Make sure to deny any unknown fragments here.
+     */
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || GeneralPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    /**
+     * This fragment shows general preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_currency_setting)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_coin_setting)));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
